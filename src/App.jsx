4eb2ef9c3b5538dccdbd893ab3c750cf70ce2120
MHS <hayaruby7909@gmail.com>
2025-12-17 13:20:39 +0900

    Feat: 학생정보리스트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './page/Main';
 import Calc from './page/Calc';
 import Dark from './page/Dark';
+import StudentInfo from './page/StudentInfo';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path={"/"} element={<Main />} />
         <Route path={"/calc"} element={<Calc />} />
         <Route path={"/dark"} element={<Dark />} />
+        <Route path={"/studentinfo"} element={<StudentInfo />} />
       </Routes>
      </BrowserRouter>
     </>
---
+++ src/page/StudentInfo.jsx
@@ -0,0 +1,10 @@
+/**
+ * 학생 정보 불러오기, json
+ */
+export default function StudentInfo() {
+    return (
+        <>
+            <h1> 학생 정보 리스트 </h1>
+        </>
+    )
+}
