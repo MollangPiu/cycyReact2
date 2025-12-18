bafe56ea19262feeef1ca0138d801d923177e5ee
MHS <hayaruby7909@gmail.com>
2025-12-18 10:17:15 +0900

    Feat: 컴포넌트 props

--- src/page/7일차_comp/자식건네주기.jsx
+++ src/page/7일차_comp/자식건네주기.jsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+
 export default function Study() {
 
     let msg = 'Hello World';
     let obj = {title: '책 제목', content: '책 내용'};
+    const [카운트, set카운트] = useState(0);
 
     return (
         <div>
             <h1>자식과 부모의 데이터 전달</h1>
             <자식 dataMsg={msg} dataBook={obj} />
+            <자식2 dataCount={카운트} />
+            <input type="button" value="숫자 증가" 
+            onClick={() => {
+                set카운트(카운트+1);
+            }}
+            />
+        </div>
+    )
+}
+
+function 자식2(props) {
+    return (
+        <div style={{width: '100vw'
+        , height: '200px'
+        , border: '2px blue solid'}}>
+            <h3>자식 Component 영역</h3>
+            {props.dataCount}
         </div>
     )
 }
