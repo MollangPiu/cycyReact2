e81cd4b194b1f4e18adef63cfc616b52e3ea7c80
MHS <hayaruby7909@gmail.com>
2025-12-23 11:01:39 +0900

    Feat: axios 기능

--- src/page/axios/Axios보내기.jsx
+++ src/page/axios/Axios보내기.jsx
@@ -9,11 +9,57 @@ export default function Study() {
         });
     }
 
+    function test2() {
+        axios.get('http://localhost:8080/data1', {params: {'text':'hello'}})
+        .then(res => {
+            console.log(res);
+        });
+    }
+
+    function test3() {
+        axios.get('http://localhost:8080/data2', {params: {'name':'홍길동', 'age': 20}})
+        .then(res => {
+            console.log(res);
+        });
+    }
+
+    function test4() {
+        axios.get('http://localhost:8080/data3',
+            {params: {'num1':100, 'num2': 20}})
+        .then(res => {
+            console.log(res);
+        });
+    }
+
+    function post1() {
+        axios.post('http://localhost:8080/post1', {name:'홍길동', age: 20})
+        .then(res => {
+            console.log(res);
+        });
+    }
+
+    function res1() {
+        axios.get('http://localhost:8080/res1', {params: {'name':'홍길동', 'age': 20}})
+        .then(res => {
+            console.log(res);
+        });
+    }
+
     return (
         <div>
             <h1>Axios 사용해보기</h1>
+            <input type="button" value="res1"
+            onClick={res1} />
+            <input type="button" value="post1"
+            onClick={post1} />
             <input type="button" value="axios보내기1"
             onClick={test1} />
+            <input type="button" value="axios보내기2"
+            onClick={test2} />
+            <input type="button" value="axios보내기3"
+            onClick={test3} />
+            <input type="button" value="axios보내기4"
+            onClick={test4} />
         </div>
     )
 }
