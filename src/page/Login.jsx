ef05e7df4a1dcc992eeb008f9e1f0f5673fd5d57
MHS <hayaruby7909@gmail.com>
2025-12-26 11:12:16 +0900

    login

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,7 +1,23 @@
+import axios from 'axios';
+
 export default function Login() {
+    function 로그인() {
+        const userId = document.getElementById('id');
+        const userPw = document.getElementById('pw');
+        const obj = {userId: userId.value, userPw: userPw.value};
+        console.log(obj);
+
+        axios.post('http://localhost:8080/api/member/login', obj)
+        .then(res=> {
+            console.log(res);
+        });
+    }
     return (
         <div>
             <h1>로그인 페이지</h1>
+            ID: <input type="text" id="id"/><br/>
+            PW: <input type="password" id="pw"/><br/>
+            <input type="button" value="로그인" onClick={로그인}/>
         </div>
     )
 }
